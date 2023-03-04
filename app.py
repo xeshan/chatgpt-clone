753a8b497a2fc724fa1373d8ee280ae319ec2d82
xeshan <xeshan.ahmed@gmail.com>
2023-03-04 16:31:05 +0500

    refactored the openai response to function

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 start_sequence = "\nAI:"
 restart_sequence = "\nHuman: "
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ",
-  temperature=0.9,
-  max_tokens=150,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0.6,
-  stop=[" Human:", " AI:"]
-)
+prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: "
+
+def openai_api(prompt):
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
+  )
+
+  return response.choices[0].text
